be73427fb33c450da9dc2751246773b663528058
Gavrilenko <gavrilenko.georgi@gmail.com>
2019-01-18 18:43:23 +0200

    Delete price position button logic added

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -5,6 +5,20 @@ export default {
   setPrices (state, payload) {
     state.pricesXp = payload
   },
+  deletePrice (state, payload) {
+    let indexOfResult = state.pricesXp.findIndex(service => service.id === payload.id)
+    let service = state.pricesXp[indexOfResult]
+    let indexOfDataToRemove = service.services.findIndex(priceString => priceString === payload.priceValue)
+    service.services.splice(indexOfDataToRemove, 1) // service services? rly?
+    // console.log(`Deleting item - ${payload.priceValue}`)
+    // console.log(`Doc id is - ${payload.id}`)
+    // console.log(`Array to edit is ${state.pricesXp}`)
+    // let arrayWithServices = state.pricesXp
+    // console.log(`Found index in array with id ${indexOfResult}`)
+    // console.log(`Will delete from -> ${service}`)
+    // console.dir(service)
+    // console.log(`Removing data with index ${indexOfDataToRemove}`)
+  },
   updatePrices (state, data) {
     console.log(`Updating state`)
     console.log(data)
@@ -12,8 +26,6 @@ export default {
     for (let service of state.pricesXp) {
       if (service.id === data.id) {
         console.log(`Saving data!`)
-      } else {
-        console.log(`Something went wrong`)
       }
     }
   }
--- src/views/Cms.vue
+++ src/views/Cms.vue
@@ -1,4 +1,5 @@
 <template>
+<!-- Price editor -->
   <v-container pa-0>
     <v-layout wrap justify-center>
 <!-- Header -->
@@ -6,7 +7,7 @@
         <h1>{{ header }}</h1>
       </v-flex>
 <!-- Expansion panel -->
-      <v-flex xs11>
+      <v-flex xs12>
         <v-expansion-panel>
           <v-expansion-panel-content
             v-for="(item,i) in 1"
@@ -18,23 +19,31 @@
               <!--v-card-text>Тут можна редагувати ціни</v-card-text-->
                   <v-flex xs12 v-for="doc in serviceItemsFromDB" :key="doc.id">
                     <v-layout wrap>
-                      <v-flex xs12>
-                        {{ doc.header }}
+                      <v-flex xs12 class="text-xs-center">
+                        <h4>{{ doc.header }}</h4>
                       </v-flex>
-                      <v-flex py-1 pl-2 xs12 v-for="(priceValue, index) in doc.services" :key="index">
-                        <v-btn icon small fab @click="editPrice(priceValue, index, doc.id)">
-                          <v-icon small color="purple">create</v-icon>
-                        </v-btn>
-                        <v-btn icon small fab>
-                          <v-icon small color="purple">delete</v-icon>
-                        </v-btn>
-                        {{ priceValue }}
+                      <v-flex xs12 v-for="(priceValue, index) in doc.services" :key="index">
+                        <v-layout row align-center>
+                          <v-flex class="text-xs-right">
+                            <v-btn icon small fab @click="editPrice(priceValue, index, doc.id, doc.header)">
+                              <v-icon small color="purple">create</v-icon>
+                            </v-btn>
+                          </v-flex>
+                          <v-flex xs9>
+                            {{ priceValue }}
+                          </v-flex>
+                          <v-flex xs2>
+                            <v-btn icon small fab @click="handlePriceDelete(doc.id, priceValue)">
+                              <v-icon small color="purple">delete</v-icon>
+                            </v-btn>
+                          </v-flex>
+                        </v-layout>
                       </v-flex>
                     </v-layout>
 
                     <v-layout row wrap>
 
-                      <v-flex xs12 sm6>
+                      <!--v-flex xs12 sm6>
                         <v-text-field
                           v-if="buffer"
                           v-model="buffer.priceValue"
@@ -44,7 +53,7 @@
                           label="Edit here"
                           solo
                         ></v-text-field>
-                      </v-flex>
+                      </v-flex-->
 
                     </v-layout>
                   </v-flex>
@@ -66,6 +75,105 @@
           </v-expansion-panel-content>
         </v-expansion-panel>
       </v-flex>
+<!-- Edit dialog -->
+      <v-flex class="text-xs-center">
+        <v-dialog
+          v-model="dialog"
+          width="500">
+          <!--v-btn
+            slot="activator"
+            color="red lighten-2"
+            dark>
+            Click Me
+          </v-btn-->
+
+          <v-card>
+            <v-card-title
+              class="grey lighten-2">
+              <h4 class="blue-grey--text">{{ buffer.docHeader }}</h4>
+            </v-card-title>
+
+            <v-card-text>
+              <v-form ref="priceEditingForm" v-model="priceEditingFormValid">
+                <v-text-field
+                  v-model="buffer.priceValue"
+                  :rules="priceStringRules"
+                  type='text'
+                  autocomplete='off'
+                  required
+                  append-icon='create'
+                  :label="buffer.valueForLabel">
+                </v-text-field>
+              </v-form>
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-actions>
+              <v-btn
+                color="purple"
+                flat
+                @click="saveData">
+                зберегти
+              </v-btn>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="grey"
+                flat
+                @click="dialog = false">
+                скасувати
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-flex>
+<!-- Delete warning dialog -->
+      <v-flex class="text-xs-center">
+        <v-dialog
+          v-model="deleteWarningDialog"
+          persistent
+          lazy
+          width="500">
+
+          <v-card>
+            <v-card-title
+              class="red lighten-3">
+              <h3 class="white--text">Увага</h3>
+            </v-card-title>
+
+            <v-card-text class="text-xs-left">
+              Дійсно видалити цю позицію? <br />
+              <span class="orange--text">{{ buffer.priceValue }}</span>
+              <!--v-text-field
+                v-model="buffer.priceValue"
+                @click:append="saveData"
+                append-icon='done'
+                :label="buffer.valueForLabel">
+              </v-text-field-->
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="red"
+                flat
+                outline
+                @click="handlePriceDelete()">
+                видалити
+              </v-btn>
+              <v-btn
+                color="primary"
+                flat
+                outline
+                @click="toggleSafeCheck">
+                скасувати
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
@@ -77,7 +185,21 @@ import db from '@/components/firebaseInit'
 export default {
   data: () => ({
     header: 'Редагувати',
-    buffer: null,
+    buffer: {
+      docHeader: '',
+      priceValue: '',
+      valueForLabel: '',
+      index: null,
+      docID: null
+    },
+    priceEditingFormValid: false,
+    priceStringRules: [
+      v => !!v || 'Необхідно ввести назву та ціну',
+      v => (v && v.length <= 60) || 'Позиція повинна бути менше 60 символів'
+    ],
+    dialog: false,
+    deleteWarningDialog: false,
+    safeToDelete: false,
     serviceItemsRef: db.collection('serviceItems'),
     serviceItemsFromDB: []
   }),
@@ -93,10 +215,48 @@ export default {
     ])
   },
   methods: {
+    handlePriceDelete (docID, priceValue) {
+      if (this.safeToDelete) {
+        console.log(`All set, deleting from store.`)
+        // console.log(`Deleting price item with id ${this.buffer.docID}`)
+        this.deletePriceFromStore().then(() => {
+          console.log(`Syncing with db..`)
+        })
+          .catch(function (error) {
+            console.log(`Error is ${error}`)
+          })
+        this.toggleSafeCheck()
+        // this.deleteWarningDialog = !this.deleteWarningDialog
+      } else {
+        console.log(`Check if is it safe with id ${docID}`)
+        this.deleteWarningDialog = !this.deleteWarningDialog
+        this.buffer.priceValue = priceValue
+        this.buffer.docID = docID
+        this.toggleSafeCheck()
+        this.deleteWarningDialog = true
+        return true
+      }
+    },
+    toggleSafeCheck () {
+      this.safeToDelete = !this.safeToDelete
+      this.deleteWarningDialog = !this.deleteWarningDialog
+    },
+    deletePriceFromStore () {
+      return new Promise(resolve => {
+        console.log(`Deleting price from store ${this.buffer.priceValue}`)
+        let payload = {
+          id: this.buffer.docID,
+          priceValue: this.buffer.priceValue
+        }
+        this.$store.commit('deletePrice', payload)
+        resolve()
+        // this.deleteWarningDialog = !this.deleteWarningDialog
+      })
+    },
     saveData () {
       console.log('Saving data.')
       // console.dir(this.buffer)
-      if (this.buffer) { // better check what you are sending
+      if (this.priceEditingFormValid) { // better check what you are sending
         // console.log(`Saving to db type of this.serviceItemsFromDb is ${this.serviceItemsFromDB[0].services}.`)
         let itemToUpdate = this.serviceItemsFromDB.find(serviceItem => serviceItem.id === this.buffer.docID)
         console.log(`Item to update is ${itemToUpdate.services[this.buffer.index]}`)
@@ -107,16 +267,20 @@ export default {
         console.log(`Data to save is ${this.buffer.priceValue}`)
         itemToUpdate.services[this.buffer.index] = this.buffer.priceValue
         console.log(`Item after update is ${itemToUpdate.services[this.buffer.index]}`)
-        this.$store.commit('updatePrices', itemToUpdate, this.buffer.docID)
+        // this.$store.commit('updatePrices', itemToUpdate, this.buffer.docID)
+        this.dialog = !this.dialog
       } else {
         console.log(`Nothing to update`)
       }
     },
-    editPrice (priceValue, index, docID) {
+    editPrice (priceValue, index, docID, docHeader) {
       console.log(`Editing price with index ${index} and id ${docID}`)
+      this.dialog = !this.dialog
       console.dir(priceValue)
       this.buffer = {
+        docHeader: docHeader,
         priceValue: priceValue,
+        valueForLabel: priceValue,
         index: index,
         docID: docID
       }
