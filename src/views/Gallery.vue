19a13ba466c8e5dfb3fd59f02e77ecc689588e4b
Gavrilenko <gavrilenko.georgi@gmail.com>
2019-05-06 13:25:56 +0300

    Instagram gallery component updated

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,17 +1,20 @@
 <template>
   <v-container fill-height>
-  <v-layout id="gallery" column>
-    <v-flex>
-      <h1 class="mt-4">{{ header }}</h1>
-    </v-flex>
-    <v-layout id="instafeed" column>
+    <v-layout wrap justify-center>
+      <v-flex xs12>
+        <h1 class="mt-4">{{ header }}</h1>
+      </v-flex>
+      <v-flex xs12 sm10 md6>
+        <v-layout id="instafeed" column>
+        </v-layout>
+      </v-flex>
     </v-layout>
-  </v-layout>
   </v-container>
 </template>
 
 <script>
 import Instafeed from '../../node_modules/instafeed.js/instafeed.min.js'
+import { userId, accessToken } from '../js/instagramCreds.js'
 
 export default {
   name: 'Gallery',
@@ -20,13 +23,12 @@ export default {
       header: 'Инстаграм'
     }
   },
-  mounted: function () {
-    this.$nextTick(function () {
-      // console.log(`Mounted`)
+  mounted () {
+    this.$nextTick(() => {
       var feed = new Instafeed({
         get: 'user',
-        userId: '6864338820', // remove this
-        accessToken: '6864338820.1677ed0.3a92f8ce375f4aaca9d935ae1f92c41e',
+        userId: userId,
+        accessToken: accessToken,
         resolution: 'standard_resolution',
         template: '<v-flex class="pa-4"><a href="{{link}}" target="_blank" id="{{id}}"><img class="instagram-img" src="{{image}}" /></a></v-flex>',
         sortBy: 'most-recent',
